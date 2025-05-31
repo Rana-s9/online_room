67c21e2512a226c796cfa434762caae4cbf70201
rana <nomumumu561@gmail.com>
2025-05-31 16:22:17 +0900

    add/edit_delete_spot

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
     resources :state_calendars, only: %i[new create index update edit destroy]
     resources :invitation_tokens, only: %i[create index update]
     resources :greetings, only: %i[new create index edit update destroy]
-    resources :spots, only: %i[index new create show]
+    resources :spots, only: %i[index new create show edit update destroy]
   end
 
   resources :areas, only: %i[create update index]
