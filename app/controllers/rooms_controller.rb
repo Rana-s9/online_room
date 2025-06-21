8d7dab95ed0df1998498ba19992eea28cd9539e1
rana <nomumumu561@gmail.com>
2025-06-21 18:06:45 +0900

    feature51/fix_welcome_message

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -42,7 +42,6 @@ class RoomsController < ApplicationController
     @greetings = @room.greetings.includes(:user).order(created_at: :desc)
     @welcome = @greetings.welcome
     @return = @greetings.return
-    @roommates_except_self = current_user.roommates_except_self(@room)
 
     if params[:from_home_button]
       flash[:just_signed_in] = true
