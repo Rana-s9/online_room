c1406c162186f5be4f342f7de0ddb6f8f73e5c20
rana <nomumumu561@gmail.com>
2025-06-29 18:06:39 +0900

    add_test

--- test/controllers/answers_controller_test.rb
+++ test/controllers/answers_controller_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class AnswersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  def setup
+    @user = User.create!(name: "TestUserFour", email: "test4@example.com", password: "password3")
+    @post = Post.create!(user: @user, relationship: 0, post_type: 0, situation: 0, content: "test", display_name: 0)
+    @answer = Answer.create!(user: @user, post: @post, body: "comments")
+
+    sign_in @user
+  end
+
+  test "should get index" do
+    get post_url(@post)
+    assert_response :success
+  end
 end
