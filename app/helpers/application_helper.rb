082043acd5ebbb4de8a2b77fcc23cbd70cc23e82
rana <nomumumu561@gmail.com>
2025-07-08 17:07:23 +0900

    update/OGP_share_image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,10 +5,10 @@ module ApplicationHelper
   def default_meta_tags
     {
       site: "Online Room",
-      title: "遠くにいる人と、日常を一緒に過ごすアプリ",
+      title: "遠くにいる人と、ちいさな日常を一緒に過ごすアプリ",
       reverse: true,
       charset: "utf-8",
-      description: "Online Roomでは、遠くにいる人や会えない人と日々の記録を共有し、同じ部屋に帰宅する体験ができます。",
+      description: "Online Roomでは、遠くにいる人や会えない人と日々の記録を共有し、同じ部屋に帰宅する体験ができます🏠",
       keywords: "遠距離恋愛,友人,家族,オンライン,交換日記,心身の記録,天気共有",
       canonical: "https://our-onlineroom.com/",
       separator: "|",
@@ -17,7 +17,7 @@ module ApplicationHelper
         title: :title,
         description: :description,
         type: "website",
-        url: "https://our-onlineroom.com/?t=refresh",
+        url: "https://our-onlineroom.com/",
         image: "https://our-onlineroom.com/images/ogp2.png",
         local: "ja-JP"
       },
