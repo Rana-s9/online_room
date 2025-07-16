6bcdba18cfdfbb0cd148bc8d41581307b7ffa1b3
rana <nomumumu561@gmail.com>
2025-07-16 15:22:40 +0900

    add/profile_show_edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   # allow_browser versions: :modern
   before_action :set_locale
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
@@ -13,5 +14,12 @@ class ApplicationController < ActionController::Base
     else
       { locale: I18n.locale }
     end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :name ]) # ←これがnameの編集に必要！
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "users/show"
   get "rooms/index"
   get "exchange_diaries/index"
 
@@ -11,6 +12,8 @@ Rails.application.routes.draw do
     sessions: "users/sessions",
     registrations: "users/registrations"
     }
+
+    resource :user, only: %i[show]
     get "top/index"
     # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
     resources :rooms, only: %i[index show create] do
