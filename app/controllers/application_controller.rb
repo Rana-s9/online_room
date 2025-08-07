7d9c363735710fe268f4c9a99d01b26e50c83b70
rana <nomumumu561@gmail.com>
2025-08-07 20:16:08 +0900

    update_link_entrance

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :update_last_seen, if: :user_signed_in?
 
+  def after_sign_in_path_for(resource)
+    rooms_path
+  end
+
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
@@ -25,6 +29,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
-    devise_parameter_sanitizer.permit(:account_update, keys: [ :name ]) # ←これがnameの編集に必要！
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :name ])
   end
 end
