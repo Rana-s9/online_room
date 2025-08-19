f63267bac74f5f82b5919e2716c473b47e5b1501
rana <nomumumu561@gmail.com>
2025-08-20 00:33:14 +0900

    cronjob_search

--- run_google_events.sh
+++ run_google_events.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+set -x  # ← 各コマンドを実行前に表示
+exec > >(tee -a /rails/log/cron.log) 2>&1
+
 export RAILS_ENV=production
 export PATH=/usr/local/bundle/bin:$PATH
-cd /rails
-bundle exec rake google_events:delete_old >> log/cron.log 2>&1
+
+cd /rails || exit 1
+echo ">>> Current directory: $(pwd)"
+echo ">>> Whoami: $(whoami)"
+
+bundle exec rake google_events:delete_old
+echo ">>> Done at $(date)"
